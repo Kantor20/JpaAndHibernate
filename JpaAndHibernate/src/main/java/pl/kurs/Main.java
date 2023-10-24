4cd9da6799b3f5ba8ac29c29ab6eadace5c413b8
Kantor20 <139916394+Kantor20@users.noreply.github.com>
2023-10-24 09:38:49 +0200

    Add files via upload

---
+++ JpaAndHibernate/src/main/java/pl/kurs/Main.java
@@ -0,0 +1,25 @@
+package pl.kurs;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            ORM - object relational mapping
+
+            java app -> object -> orm -> db
+
+            JPA - java persistence API
+
+            jpa layer -> zbior interfejsow
+
+            hibernate -> przyklad ORMa
+
+            POJO - plain old java object
+
+            encja (entity)
+
+            entity manager
+            entity manager factory
+
+         */
+    }
+}
